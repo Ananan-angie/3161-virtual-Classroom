c074665f3e5daa087af3fcbdc152d1dd07d659df
Jingcheng Xu <jxuu0015@student.monash.edu>
2021-08-06 16:06:26 +0800

    Add ButtonEvent Script

---
+++ Assets/Jingcheng Xu/ButtonEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public GameObject map;
+
+    public void ChangeToScene(string sceneNo)
+    {
+        Application.LoadLevel(sceneNo);
+    }
+
+    public void OpenCloseMap()
+    {
+        if (map.active)
+        {
+            map.SetActive(false);
+        }
+        else {
+            map.SetActive(true);
+        }
+    }
+
+    public void PrintButtonName(GameObject button)
+    {
+        print("Hello from: " + button.name);
+    }
+}
