62794a089f43db38bb3da55c4009a2f8ba40754d
jx980 <separate980@hotmail.com>
2021-08-09 15:26:02 +1000

    Update ExpandButton.cs

--- Assets/Junxian Wang/Scripts/ExpandButton.cs
+++ Assets/Junxian Wang/Scripts/ExpandButton.cs
@@ -5,49 +5,47 @@ using UnityEngine;
 public class ExpandButton : MonoBehaviour
 {
     [SerializeField] private string directionToCollapse = "up";
-    [SerializeField] private GameObject parentWindow;
-    [SerializeField] private GameObject button;
-    private float parentHeight, parentWidth, buttonHeight, buttonWidth;
-    private GameObject controlledContent;
+    [SerializeField] private GameObject controlledWindow;
+    private float windowHeight, windowWidth, buttonHeight, buttonWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        parentHeight = parentWindow.GetComponent<RectTransform>().rect.height;
-        parentWidth = parentWindow.GetComponent<RectTransform>().rect.width;
-        buttonHeight = button.gameObject.GetComponent<RectTransform>().rect.height;
-        buttonWidth = button.gameObject.GetComponent<RectTransform>().rect.width;
-        controlledContent = parentWindow.transform.Find("Window Container").gameObject;
+        windowHeight = controlledWindow.GetComponent<RectTransform>().rect.height;
+        windowWidth = controlledWindow.GetComponent<RectTransform>().rect.width;
+        buttonHeight = gameObject.GetComponent<RectTransform>().rect.height;
+        buttonWidth = gameObject.GetComponent<RectTransform>().rect.width;
     }
+
 
     public void ToggleExpansion()
 	{
-        bool isActive = !controlledContent.activeSelf;
-        controlledContent.SetActive(isActive);
+        bool isActive = !controlledWindow.activeSelf;
+        controlledWindow.SetActive(isActive);
         float deltaX = 0, deltaY = 0;
 
         if (directionToCollapse == "up")
 		{
             deltaX = 0;
-            deltaY = isActive ? -parentHeight + buttonHeight : parentHeight - buttonHeight;
+            deltaY = isActive ? -windowHeight + buttonHeight : windowHeight - buttonHeight;
 		}
         else if (directionToCollapse == "down")
 		{
             deltaX = 0;
-            deltaY = isActive ? parentHeight - buttonHeight : -parentHeight + buttonHeight;
+            deltaY = isActive ? windowHeight - buttonHeight : -windowHeight + buttonHeight;
         }
         else if (directionToCollapse == "left")
         {
-            deltaX = isActive ? parentWidth - buttonWidth : -parentWidth + buttonWidth;
+            deltaX = isActive ? windowWidth - buttonWidth : -windowWidth + buttonWidth;
             deltaY = 0;
         }
         else if (directionToCollapse == "right")
         {
-            deltaX = isActive ? -parentWidth + buttonWidth : parentWidth - buttonWidth;
+            deltaX = isActive ? -windowWidth + buttonWidth : windowWidth - buttonWidth;
             deltaY = 0;
         }
 
-        button.gameObject.transform.position = new Vector3(button.gameObject.transform.position.x + deltaX, button.gameObject.transform.position.y + deltaY);
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x + deltaX, gameObject.transform.position.y + deltaY);
 
     }
 }
