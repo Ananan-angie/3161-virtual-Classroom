5340f1f2dea15cfa746bfc4c09a05bc3dc1bb9e6
jx980 <separate980@hotmail.com>
2021-08-13 18:44:35 +1000

    Update animation, fix normal view

--- Assets/Junxian Wang/Scripts/PlayerController.cs
+++ Assets/Junxian Wang/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] GameObject playerSprite;
-    [SerializeField] Animator animator;
+    [SerializeField] GameObject playerAnimatorParent;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = playerAnimatorParent.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,12 +22,17 @@ public class PlayerController : MonoBehaviour
 		float horiInput = Input.GetAxis("Horizontal");
 		float vertInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horiInput * Time.fixedDeltaTime * speed, vertInput * Time.fixedDeltaTime * speed, 0);
-
-        /* Change facing */
-        playerSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, movement);
         gameObject.transform.Translate(movement);
 
-        /* Animation */
-        animator.SetFloat("speed", movement.magnitude);
+		/* Change facing */
+		if (movement.magnitude > 0)
+		{
+			playerSprite.transform.rotation = Quaternion.LookRotation(Vector3.forward, movement);
+
+		}
+
+
+		/* Animation */
+		animator.SetFloat("speed", movement.magnitude);
     }
 }
