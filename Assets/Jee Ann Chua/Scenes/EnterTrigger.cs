4e7010d3bb61a5f0e98930213f72f1f3e4a42eeb
jeeannt <chuajeeann@gmail.com>
2021-08-15 16:05:34 +1000

    Create EnterTrigger

---
+++ Assets/Jee Ann Chua/Scenes/EnterTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterTrigger : MonoBehaviour
+{
+    public int Scene;
+    private bool hasPlayer;
+ 
+    private void Update() {
+        if (hasPlayer && Input.GetKeyDown(KeyCode.E)) {
+            SceneManager.LoadScene(Scene);
+        }
+    }
+ 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            hasPlayer = true;
+        }
+    }
+ 
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            hasPlayer = false;
+        }
+    }
+}
