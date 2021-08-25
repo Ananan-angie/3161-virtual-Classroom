ed81919998c9a4147a9e3adb05c88225917b9d38
Jingcheng Xu <jxuu0015@student.monash.edu>
2021-08-25 14:45:11 +0800

    Chat component

---
+++ Assets/1-Jingcheng Xu/ChatManager.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    public string usrname;
+    public int maxMessages = 25;
+    public GameObject textObject,chatPanel;
+    public TMP_InputField chatBox;
+    public Color Acolor, Scolor, Pcolor;
+
+    [SerializeField]
+    TMP_Dropdown dropdown;
+    [SerializeField]
+    List<string> dropitems;
+    [SerializeField]
+    List<Message> messagesList = new List<Message>();
+
+    PlayerControl controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControl();
+        controls.UI.Chating.performed += ctx => Chat();
+        dropitems.Add("All");
+        dropitems.Add("Stuff");
+        dropitems.Add("Private");
+        foreach (var item in dropitems)
+        {
+            dropdown.options.Add(new TMP_Dropdown.OptionData() { text = item });
+        }
+    }
+
+    void Chat()
+    {
+        if (chatBox.text != "")
+        {
+            if (dropitems[dropdown.value] == "All")
+            {
+                SentToChat(chatBox.text, Message.MessageType.All);
+            }
+            else if (dropitems[dropdown.value] == "Stuff")
+            {
+                SentToChat(chatBox.text, Message.MessageType.Stuff);
+            }
+            else
+            {
+                SentToChat(chatBox.text, Message.MessageType.Private);
+            }
+            chatBox.text = "";
+        }
+        else if (!chatBox.isFocused)
+        {
+            chatBox.ActivateInputField();
+        } 
+    }
+
+    public void SentToChat(string text, Message.MessageType messageType)
+    {
+        if (messagesList.Count >= maxMessages)
+        {
+            Destroy(messagesList[0].textObject.gameObject);
+            messagesList.Remove(messagesList[0]);
+        }
+
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<TMP_Text>();
+
+        newMessage.textObject.text = newMessage.text;
+        newMessage.textObject.color = MessageTypeColor(messageType);
+
+
+        messagesList.Add(newMessage);
+
+        Color MessageTypeColor(Message.MessageType messageType)
+        {
+            Color color = Acolor;
+            switch (messageType)
+            {
+                case Message.MessageType.All:
+                    color = Acolor;
+                    break;
+                case Message.MessageType.Stuff:
+                    color = Scolor;
+                    break;
+                case Message.MessageType.Private:
+                    color = Pcolor;
+                    break;
+            }
+            return color;
+        }
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    [System.Serializable]
+    public class Message
+    {
+        public string text;
+        public TMP_Text textObject;
+        public MessageType messageType;
+
+        public enum MessageType
+        {
+            All,
+            Stuff,
+            Private
+        }
+    }
+}
--- Assets/Input Control/PlayerControl.cs
+++ Assets/Input Control/PlayerControl.cs
@@ -115,6 +115,14 @@ public class @PlayerControl : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Chating"",
+                    ""type"": ""Button"",
+                    ""id"": ""8ffef716-8029-4c43-a4de-215a2322c822"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -126,6 +134,17 @@ public class @PlayerControl : IInputActionCollection, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Back"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9bfc97bb-ba15-40d6-8c1e-be260686b464"",
+                    ""path"": ""<Keyboard>/enter"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Chating"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -147,6 +166,7 @@ public class @PlayerControl : IInputActionCollection, IDisposable
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Back = m_UI.FindAction("Back", throwIfNotFound: true);
+        m_UI_Chating = m_UI.FindAction("Chating", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -238,11 +258,13 @@ public class @PlayerControl : IInputActionCollection, IDisposable
     private readonly InputActionMap m_UI;
     private IUIActions m_UIActionsCallbackInterface;
     private readonly InputAction m_UI_Back;
+    private readonly InputAction m_UI_Chating;
     public struct UIActions
     {
         private @PlayerControl m_Wrapper;
         public UIActions(@PlayerControl wrapper) { m_Wrapper = wrapper; }
         public InputAction @Back => m_Wrapper.m_UI_Back;
+        public InputAction @Chating => m_Wrapper.m_UI_Chating;
         public InputActionMap Get() { return m_Wrapper.m_UI; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -255,6 +277,9 @@ public class @PlayerControl : IInputActionCollection, IDisposable
                 @Back.started -= m_Wrapper.m_UIActionsCallbackInterface.OnBack;
                 @Back.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnBack;
                 @Back.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnBack;
+                @Chating.started -= m_Wrapper.m_UIActionsCallbackInterface.OnChating;
+                @Chating.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnChating;
+                @Chating.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnChating;
             }
             m_Wrapper.m_UIActionsCallbackInterface = instance;
             if (instance != null)
@@ -262,6 +287,9 @@ public class @PlayerControl : IInputActionCollection, IDisposable
                 @Back.started += instance.OnBack;
                 @Back.performed += instance.OnBack;
                 @Back.canceled += instance.OnBack;
+                @Chating.started += instance.OnChating;
+                @Chating.performed += instance.OnChating;
+                @Chating.canceled += instance.OnChating;
             }
         }
     }
@@ -283,5 +311,6 @@ public class @PlayerControl : IInputActionCollection, IDisposable
     public interface IUIActions
     {
         void OnBack(InputAction.CallbackContext context);
+        void OnChating(InputAction.CallbackContext context);
     }
 }
