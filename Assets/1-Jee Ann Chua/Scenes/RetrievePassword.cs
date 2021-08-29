dc85e0ddbcac354dcfa90c811950e29b8b5d1d3d
jeeannt <chuajeeann@gmail.com>
2021-08-29 17:30:07 +1000

    Update register & forgot password button

---
+++ Assets/1-Jee Ann Chua/Scenes/RetrievePassword.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.IO;
+using TMPro;
+
+public class RetrievePassword : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public TMP_InputField emailInput;
+    public TMP_Text message;
+
+    public void getPassword(){
+        UserDetails col = JsonUtility.FromJson<UserDetails>(jsonFile.text);
+        List<UserDetail> listOfDetails = col.userdetails.ToList();
+
+        bool found = false;
+        foreach(UserDetail userdetails in listOfDetails){
+            if(userdetails.username == emailInput.text){
+                message.text = "Your password is " + userdetails.password;
+                found = true;
+            }
+        }
+
+        if(!found){
+            message.text = "User not found";
+        }
+
+        message.gameObject.SetActive(true);
+    }
+}
