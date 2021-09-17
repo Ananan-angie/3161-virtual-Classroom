972337842a8e4fc6a3c9cdb59fe7f666968eeeb0
jeeannt <chuajeeann@gmail.com>
2021-09-17 21:01:00 +1000

    Update Upcoming Classes

--- Assets/1-Jee Ann Chua/Scenes/UIManager.cs
+++ Assets/1-Jee Ann Chua/Scenes/UIManager.cs
@@ -26,7 +26,9 @@ public class UIManager : MonoBehaviour
     };
 
     public TMP_Text UpcomingClass;
-
+    string name;
+    int scheduleDay;
+    int scheduleTime;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +36,9 @@ public class UIManager : MonoBehaviour
         UpdateSchedule();
     }
 
+    void Update(){
+        UpdateUpcomingClass();
+    }
 
     void UpdateSchedule(){
         for (int t = 0; t < 13; t++){
@@ -44,5 +49,72 @@ public class UIManager : MonoBehaviour
         }
     }
 
-   
+    void UpdateUpcomingClass(){
+        DateTime current = DateTime.Now;
+        int day = (int) current.DayOfWeek;
+        int hour = current.Hour + 1;
+
+        // Sunday == 0, convert sunday to 7
+        if (day == 0){
+            day = 7;
+        }
+
+        // if current hour is later than 8pm or earlier than 8am, set to 8am the next day
+        if (hour > 20 || hour < 8){
+            hour = 8;
+            day += 1;
+        }
+
+        // if exceeds Sunday, set it back to Monday
+        if (day > 7){
+            day = 1;
+        }
+
+        bool found = false;
+
+        // finds the upcoming class of current time this week
+        for (int t = 0; t < 13; t++){
+            for (int d = 1; d < 8; d++){
+                if(t + 8 >= hour && d >= day && arr2d[t,d] != ""){
+                    name = arr2d[t,d];
+                    scheduleDay = d;
+                    scheduleTime = t+8;
+                    found = true;
+                    goto End;
+                }
+            }
+        }
+        
+        // if no upcoming class this week, finds the first class of the week
+        if(!found){
+            for (int t = 0; t < 13; t++){
+                for (int d = 1; d < 8; d++){
+                    if(arr2d[t,d] != ""){
+                        name = arr2d[t,d];
+                        scheduleDay = d;
+                        scheduleTime = t+8;
+                        found = true;
+                        goto End;
+                    }
+                }
+            }
+        }
+    End:
+        if(found){
+            if(scheduleTime < 12){
+                UpcomingClass.text = "Day: " + Enum.GetName(typeof(DayOfWeek),scheduleDay) + "\n Time: " + scheduleTime + "AM" + "\n Class Name: " + name + "\n Class Number : 01" + "\n Tutor: ";
+            }
+            else if(scheduleTime == 12){
+                UpcomingClass.text = "Day: " + Enum.GetName(typeof(DayOfWeek),scheduleDay) + "\n Time: " + scheduleTime + "PM" + "\n Class Name: " + name + "\n Class Number : 01" + "\n Tutor: "; 
+            }
+            else{
+                scheduleTime -= 12;
+                UpcomingClass.text = "Day: " + Enum.GetName(typeof(DayOfWeek),scheduleDay) + "\n Time: " + scheduleTime + "PM" + "\n Class Name: " + name + "\n Class Number : 01" + "\n Tutor: "; 
+            }
+            
+        }
+        else{
+            UpcomingClass.text = "No upcoming session.";
+        }
+    }
 }
